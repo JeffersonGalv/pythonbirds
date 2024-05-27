c0a2ca0f89bdc2b4f3f1cbd83de13c5511f27f88
JeffersonGalv <jefdhenes@gmail.com>
2024-05-27 13:28:06 -0300

    Implementada Direção

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,21 @@
 class Pessoa:
     olhos=2
+
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
 
-
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f"{cls} - olhos {cls.olhos}"
 
 if __name__ == '__main__':
     renzo = Pessoa(nome='Renzo')
@@ -22,12 +29,14 @@ if __name__ == '__main__':
         print(filho.nome)
     luciano.sobrenome = "Ramalho"
     del luciano.filhos
-    Pessoa.olhos = 3
     luciano.olhos = 1
     del luciano.olhos
     print(luciano.__dict__)
     print(renzo.__dict__)
+    Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(luciano.olhos)
     print(renzo.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
